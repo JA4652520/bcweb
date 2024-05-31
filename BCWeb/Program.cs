eaa180a729bcda47d799cb58ca822536250e9b9c
bc <JA4652520@gmail.com>
2024-05-31 10:41:34 +0800

    修改json和Program內容

--- BCWeb/Program.cs
+++ BCWeb/Program.cs
@@ -5,6 +5,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews()
     .AddNewtonsoftJson();
+builder.Services.AddSingleton<LineBotConfig, LineBotConfig>((s)=> new LineBotConfig
+{
+    channelSecret = builder.Configuration.GetSection("LineBot")["channelSecret"]!,
+    accessToken = builder.Configuration.GetSection("LineBot")["accessToken"]!,
+});
+
+builder.Services.AddSingleton<ILineService, LineService>();
+builder.Services.AddHttpContextAccessor();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,7 +26,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-    
+
 app.UseRouting();
 
 app.UseAuthorization();
