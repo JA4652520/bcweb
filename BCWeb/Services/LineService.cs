828f9ff9281de2395f353f1885f6884aff54f1ec
bc <JA4652520@gmail.com>
2024-05-31 11:42:33 +0800

    Add yt音樂內容

--- BCWeb/Services/LineService.cs
+++ BCWeb/Services/LineService.cs
@@ -19,17 +19,105 @@ public class LineService : ILineService
             };
             return result;
         }
-        if (message.Contains("新造的人"))
+        if (message.Contains("Someone You Loved"))
         {
             result = new List<ISendMessage>
             {
-                new TextMessage("https://youtu.be/VjF_mjBVK6c?si=4LCsQNonAUYsMh-S")
+                new TextMessage("https://youtu.be/SxJmz6b5d18?si=AcSpsyynRAQOaHeU")
+            };
+            return result;
+        }
+        if (message.Contains("Push"))
+        {
+            result = new List<ISendMessage>
+            {
+                new TextMessage("https://youtu.be/MV045tu4tN0?si=TYylzMqf9PMfkTQ2")
+            };
+            return result;
+        }
+        if (message.Contains("挪威的森林"))
+        {
+            result = new List<ISendMessage>
+            {
+                new TextMessage("https://youtu.be/IMJxnPP883Y?si=QzU4COeJo_3kmA73")
+            };
+            return result;
+        }
+        if (message.Contains("LIVE FOR YOU"))
+        {
+            result = new List<ISendMessage>
+            {
+                new TextMessage("https://youtu.be/NTrkNUziJdI?si=FZHBbJLdE79Iq6D8")
+            };
+            return result;
+        }
+        if (message.Contains("如果可以"))
+        {
+            result = new List<ISendMessage>
+            {
+                new TextMessage("https://youtu.be/Z7_hI1AxEKg?si=dlWrdg-miBKiCmgt")
+            };
+            return result;
+        }
+        if (message.Contains("Champion"))
+        {
+            result = new List<ISendMessage>
+            {
+                new TextMessage("https://youtu.be/eNWTBimUDuk?si=2o5aOeclYmAIgPW1")
+            };
+            return result;
+        }
+        if (message.Contains("Just Me"))
+        {
+            result = new List<ISendMessage>
+            {
+                new TextMessage("https://youtu.be/exbZfNR10Ak?si=bh4xv79eYduF53E7")
+            };
+            return result;
+        }
+        if (message.Contains("Sunflower"))
+        {
+            result = new List<ISendMessage>
+            {
+                new TextMessage("https://youtu.be/x_7Rz2tB6ac?si=Wbp5Rk3nm5bD3W7m")
+            };
+            return result;
+        }
+        if (message.Contains("Sextasy"))
+        {
+            result = new List<ISendMessage>
+            {
+                new TextMessage("https://youtu.be/IRYliuNMO7c?si=0KnIejhMsM5MJSpS")
+            };
+            return result;
+        }
+        if (message.Contains("Stay"))
+        {
+            result = new List<ISendMessage>
+            {
+                new TextMessage("https://youtu.be/cTl6Mq8vErk?si=9iQ6ekCJsBhFebm2")
+            };
+            return result;
+        }
+        if (message.Contains("Light Switch"))
+        {
+            result = new List<ISendMessage>
+            {
+                new TextMessage("https://youtu.be/D5uB048uBa0?si=FImgD70Gk8Jqh_SC")
+            };
+            return result;
+        }
+        if (message.Contains("My City"))
+        {
+            result = new List<ISendMessage>
+            {
+                new TextMessage("https://youtu.be/GOC2YjU0Nyk?si=Dxqo1-PtPqy7My9N")
             };
             return result;
         }
         result = new List<ISendMessage>
         {
-            new TextMessage("你是講啥小?")
+            new TextMessage("你是講啥小?\n是在靠北喔?"),
         };
         return result;
     }
